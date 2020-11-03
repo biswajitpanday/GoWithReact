1e21045fcfa906111db078a6ea6d91c7c8bc03d3
biswajitmailid <biswajitmailid@gmail.com>
2020-11-03 19:10:25 +0000

    Home & Login Component Create.
    Routing Between Home & Login.

--- reactjs-client/src/App.tsx
+++ reactjs-client/src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from './features/login/login';
+import Home from './features/home/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <BrowserRouter>
+              <Switch>
+                <Route path="/login" component={Login}></Route>
+                <Route path="/" component={Home}></Route>
+              </Switch>
+            </BrowserRouter>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
