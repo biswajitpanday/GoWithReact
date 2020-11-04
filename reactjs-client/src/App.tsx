357fbb101cfb7c3966d6598e82c7e9c465c2c39b
biswajitmailid <biswajitmailid@gmail.com>
2020-11-04 20:58:18 +0000

    Registration Form Create.

--- reactjs-client/src/App.tsx
+++ reactjs-client/src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './features/home/home';
 import Login from './auth/login/login';
+import Registration from './auth/registration/registration';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
+          <Route path="/registration" component={Registration}></Route>
           <Route path="/login" component={Login}></Route>
           <Route path="/" component={Home}></Route>
         </Switch>
