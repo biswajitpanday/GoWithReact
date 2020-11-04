44f72613c679a47918488fb45adbb43cd8a9895f
biswajitmailid <biswajitmailid@gmail.com>
2020-11-04 20:53:20 +0000

    Forget Password Component create.

---
+++ reactjs-client/src/auth/forgetPassword/forgetPassword.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ForgetPassword extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Forget Password Page</h1>
+            </div>
+        )
+    }
+}
