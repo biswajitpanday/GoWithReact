7716212863bcfdfcebedcae0879b9b5b86e339dd
biswajitmailid <biswajitmailid@gmail.com>
2020-11-04 20:52:57 +0000

    Move Login component inside auth feature. Login form create..

--- reactjs-client/src/App.tsx
+++ reactjs-client/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Login from './features/login/login';
 import Home from './features/home/home';
+import Login from './auth/login/login';
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <BrowserRouter>
-              <Switch>
-                <Route path="/login" component={Login}></Route>
-                <Route path="/" component={Home}></Route>
-              </Switch>
-            </BrowserRouter>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/" component={Home}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
