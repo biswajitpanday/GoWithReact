e8b0ffae8235aca48de21ea183ed36fe1849b14c
biswajitmailid <biswajitmailid@gmail.com>
2020-11-11 20:51:18 +0000

    HttpHelper class (Axios wrapper).
    Get, Post & UploadImage wrapper.
    Axios install.

---
+++ reactjs-client/src/helpers/httpHelpers.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+export class HttpHelpers {
+
+    static async get<T>(api: string): Promise<T> {
+        return new Promise((resolve, reject) => {
+            axios.get(api).then((response) => {
+                resolve(response.data);
+            }).catch((err) => {
+                if(err.response)
+                    reject({ message: err.response.data.error });
+                else
+                    reject({ message: "An error occurs. Please try again later. "});
+            })
+        })
+    }
+
+    static async post<T>(api: string, body?: any): Promise<T> {
+        return new Promise((resolve, reject) => {
+            axios.post(api, body).then((response) => {
+                resolve(response.data);
+            }).catch((err) => {
+                if(err.response)
+                    reject({ message: err.response.data.error });
+                else
+                    reject({ message: "An error occurs. Please try again later. "});
+                
+            })
+        })
+    }
+
+    static async uploadImage<T>(api: string, image: any): Promise<T> {
+        return new Promise((resolve, reject) => {
+            const formData = new FormData();
+            formData.append('image', image);
+            
+            axios.post(api, formData, {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            }).then((response) => {
+                resolve(response.data);
+            }).catch((err) => {
+                if(err.response)
+                    reject({ message: err.response.data.error });
+                else
+                    reject({ message: "An error occurs. Please try again later. "});
+            })
+        })
+    }
+}
