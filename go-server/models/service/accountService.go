93875809cf1770476f9af5d7f1d55848c7dfcd59
biswajitmailid <biswajitmailid@gmail.com>
2020-11-13 19:55:07 +0000

    Add AccountService [Go Server]

--- go-server/models/service/accountService.go
+++ go-server/models/service/accountService.go
@@ -3,12 +3,13 @@ package service
 import (
 	"GoWithReact/models/entity"
 	"GoWithReact/models/viewmodels"
+	"GoWithReact/repository"
+	"GoWithReact/utils"
 	"errors"
 	"log"
 	"time"
 
-	"github.com/globalsign/mgo/bson"
-	"github.com/goonode/mogo"
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -18,14 +19,19 @@ type AccountService struct{}
 //Register is to register user
 func (accountService *AccountService) Register(register viewmodels.UserRegister) error {
 
-	doc := mogo.NewDoc(entity.Account{}).(*(entity.Account))
-	err := doc.FindOne(bson.M{"email": register.Email}, doc)
+	accountRepository := new(repository.AccountRepository)
+
+	_, err := accountRepository.FindByEmail(register.Email)
+
+	if err == nil {
+		return errors.New("Email Already Exists")
+	}
 
 	hashPassword, err := bcrypt.GenerateFromPassword([]byte(register.Password), bcrypt.MinCost)
 
 	if err == nil {
 		log.Fatal(err)
-		return errors.New("Already Exist")
+		return errors.New("Internal Server Error")
 	}
 
 	acc := &entity.Account{
@@ -36,10 +42,40 @@ func (accountService *AccountService) Register(register viewmodels.UserRegister)
 		IsEmailVerified:       false,
 		IsPhoneVerified:       false,
 		JoinDate:              time.Now().Unix(),
-		Role:                  "talent",
+		Role:                  "user",
 	}
 
-	account := mogo.NewDoc(acc).(*(entity.Account))
-	err = mogo.Save(account)
+	//account, createError :=
+	accountRepository.Create(acc)
+
+	// if createError == nil {
+	// 	return account
+	// }
+
 	return err
+}
+
+//Login ...
+func (accountService *AccountService) Login(loginModel viewmodels.LoginModel) (string, error) {
+	accountRepository := new(repository.AccountRepository)
+	user, mongoErr := accountRepository.FindByEmail(loginModel.Username)
+
+	if mongoErr != nil {
+		return "", errors.New("Invalid Username or Password!")
+	}
+
+	passwordErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginModel.Password))
+	if passwordErr != nil {
+		return "", errors.New("Invalid Username or Password")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": string(user.Email),
+	})
+
+	secretKey := utils.EnvVar("TOKEN_KEY")
+	tokenString, err := token.SignedString([]byte(secretKey))
+	log.Println(tokenString, err)
+	return tokenString, err
+
 }
