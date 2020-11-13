5947e4c9e990551209cdb0163672e39c560eba13
biswajitmailid <biswajitmailid@gmail.com>
2020-11-13 20:46:52 +0000

    Add CORS.

--- go-server/routers/index.go
+++ go-server/routers/index.go
@@ -2,11 +2,30 @@ package routers
 
 import "github.com/gin-gonic/gin"
 
+//CORS ...
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
+//InitRoute ...
 func InitRoute() *gin.Engine {
 	router := gin.New()
+	router.Use(CORS())
+
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
-	setAuthRoute(router)
+	setAuthRouters(router)
+	setCommonRouters(router)
 	return router
 }
