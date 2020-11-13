4a62d2d4c0ba6f0f858b88a13a024a04ff92c2cf
biswajitmailid <biswajitmailid@gmail.com>
2020-11-13 16:25:31 +0000

    Install mongodb driver.

--- go-server/go.mod
+++ go-server/go.mod
@@ -3,6 +3,7 @@ module GoWithReact
 go 1.15
 
 require (
+	github.com/Kamva/mgm v1.2.3
 	github.com/gin-gonic/gin v1.6.3
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/go-bongo/go-dotaccess v0.0.0-20190924013105-74ea4f4ca4eb // indirect
@@ -10,5 +11,6 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/oleiade/reflections v1.0.0 // indirect
 	github.com/stretchr/testify v1.4.0
+	go.mongodb.org/mongo-driver v1.1.3
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
 )
--- go-server/models/db/mongodb.go
+++ go-server/models/db/mongodb.go
@@ -1,25 +1,26 @@
 package db
 
 import (
-	"log"
-
-	"github.com/goonode/mogo"
+	"github.com/Kamva/mgm"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var mongoConnection *mogo.Connection = nil
+var _connectionString string = ""
+var _dbName string = ""
 
-//ConnectToDatabase connection datbase when application start
-func ConnectToDatabase(connectionString string, dbName string) {
+//SetConnectionInfo connection datbase when application start
+func SetConnectionInfo(connectionString string, dbName string) {
+	_connectionString = connectionString
+	_dbName = dbName
+}
 
-	config := &mogo.Config{
-		ConnectionString: connectionString,
-		Database:         dbName,
-	}
+//Connect ...
+func Connect() {
+	_ = mgm.SetDefaultConfig(nil, _dbName, options.Client().ApplyURI(_connectionString))
+}
 
-	mongoCon, err := mogo.Connect(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	mongoConnection = mongoCon
+//Disconnect ...
+func Disconnect() {
+	_, client, _, _ := mgm.DefaultConfigs()
+	client.Disconnect(mgm.Ctx())
 }
