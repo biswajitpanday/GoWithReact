4a6651f209ccd02f4c7ff699499321a1e3bd22c4
biswajitmailid <biswajitmailid@gmail.com>
2020-11-13 19:56:10 +0000

    HttpInterceptor add for ReactClient Project.

---
+++ reactjs-client/src/helpers/httpInterceptor.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { LocalStorageHelper } from './localStorageHelper';
+
+export class HttpInterceptor {
+    
+    static init() {
+        axios.defaults.baseURL = process.env.REACT_APP_API_BASE;
+
+        console.log(axios.defaults.baseURL);
+
+        
+        axios.interceptors.request.use(config => {
+            const accessToken = LocalStorageHelper.AccessToken;
+            if(accessToken) {
+                config.headers['Authorization'] = `Bearer ${accessToken}`;
+            }
+            config.headers['Content-Type'] = 'application/json; charset=UTF-8';
+            return config;
+        }, error => {
+            Promise.reject(error);
+        })
+    }
+}
--- reactjs-client/src/index.tsx
+++ reactjs-client/src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { store } from './helpers/rootStore';
+import { HttpInterceptor } from './helpers/httpInterceptor';
+
+HttpInterceptor.init();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,7 +19,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//reportWebVitals();
+serviceWorker.unregister();
