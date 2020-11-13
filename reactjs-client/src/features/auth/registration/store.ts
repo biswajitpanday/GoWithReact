273eeff06bd4d877e486439322a342a7fed5172f
biswajitmailid <biswajitmailid@gmail.com>
2020-11-13 21:59:21 +0000

    Registration Store.

---
+++ reactjs-client/src/features/auth/registration/store.ts
@@ -0,0 +1,76 @@
+import { IRegistrationModel, IRegistrationState } from './models';
+import { createSlice, Dispatch } from '@reduxjs/toolkit';
+import { Utility } from '../../../helpers/utility';
+import { HttpHelpers } from '../../../helpers/httpHelpers';
+import { ApiConstant } from '../../../helpers/apiConstant';
+import { Routes } from '../../../helpers/routes';
+import { push } from 'connected-react-router';
+
+export const registrationState: IRegistrationState = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    isBusy: false,
+    errorMessage: '',
+    isFormValid: false
+}
+
+const slice = createSlice({
+    name: 'registrationStore',
+    initialState: registrationState,
+    reducers: {
+        onError: (state, action) => {
+            return {
+                ...state,
+                isBusy: false,
+                errorMessage: action.payload.data
+            }
+        },
+
+        changeBusyState: (state, action) => {
+            return {
+                ...state,
+                errorMessage: '',
+                isBusy: action.payload.data
+            }
+        }
+    }
+});
+
+export default slice.reducer
+
+export const registration = (registrationModel: IRegistrationModel) => async (dispatch: Dispatch) => {
+    const validationMessage = validateModel(registrationModel)
+    if(validationMessage) {
+        dispatch(slice.actions.onError({data: validationMessage}))
+        return;
+    }
+    try {
+        dispatch(slice.actions.changeBusyState({data: true}))
+        await HttpHelpers.post<any>(ApiConstant.registration, registrationModel);
+        dispatch(push(Routes.login))
+    } catch (error) {
+        dispatch(slice.actions.changeBusyState({ data: false }))
+        dispatch(slice.actions.onError({ data: error.message }))
+    }
+}
+
+/* #region  Supported Methods */
+
+const validateModel = (registrationModel: IRegistrationModel): string => {
+    if (!Utility.ValidateEmail(registrationModel.email)) {
+        return 'Email address is not valid';
+    }
+    if (registrationModel.password.length < 8) {
+        return 'Minimum password length is 8';
+    }
+    if (registrationModel.password !== registrationModel.confirmPassword) {
+        return 'Password did not match';
+    }
+    return '';
+}
+
+/* #endregion */
+
