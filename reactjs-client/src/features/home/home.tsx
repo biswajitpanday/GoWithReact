dcdc2032fd620c4f273ef54c97b8e3e5a432a4d0
biswajitmailid <biswajitmailid@gmail.com>
2020-11-17 21:12:46 +0000

    Employee contact Basic table design.

--- reactjs-client/src/features/home/home.tsx
+++ reactjs-client/src/features/home/home.tsx
@@ -7,7 +7,49 @@ export default class Home extends Component {
         return (
             <AppLayout>
                 <TopNav></TopNav>
-                <h1>Welcome Home</h1>
+                <div className="row">
+                    <div className="col-6 offset-3 mt-5">
+                        <div>
+                            <h3 className="float-left">Employee Contacts</h3>
+                            <button type="button" className="btn btn-sm btn-outline-dark float-right" >Add New</button>
+                        </div>
+
+                        <table className="table table-bordered mt-4">
+                            <thead>
+                            <tr>
+                                <th>Firstname</th>
+                                <th>Lastname</th>
+                                <th>Email</th>
+                                <th>Mobile</th>
+                                <th>skype_id</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>John</td>
+                                <td>Doe</td>
+                                <td>john@example.com</td>
+                                <td>+8801XXXXXXXX</td>
+                                <td>john_doe</td>
+                            </tr>
+                            <tr>
+                                <td>Mary</td>
+                                <td>Moe</td>
+                                <td>mary@example.com</td>
+                                <td>++8801XXXXXXXX</td>
+                                <td>moe_mary</td>
+                            </tr>
+                            <tr>
+                                <td>July</td>
+                                <td>Dooley</td>
+                                <td>july@example.com</td>
+                                <td>++8801XXXXXXXX</td>
+                                <td>july_doley</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </AppLayout>
         )
     }
