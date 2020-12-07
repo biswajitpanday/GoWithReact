69041c670fe369f2e7bc7ea9b316d1375aa36475
biswajitmailid <biswajitmailid@gmail.com>
2020-12-07 19:40:44 +0000

    Employee model create.

---
+++ reactjs-client/src/features/employee/models.ts
@@ -0,0 +1,22 @@
+import { IBaseState } from "../../common/models";
+
+
+export interface IEmployeeModel {
+    
+}
+
+export interface IEmployeeStateModel extends IBaseState, IEmployeeModel {
+    isValidForm: boolean;
+}
+
+export interface IEmployeeProps extends IBaseState {
+    reset: () => void
+}
+
+export interface IEmployeeResponse {
+    firstName: string;
+    lastName: string;
+    email: string;
+    mobile: string;
+    skypeId: string;
+}
