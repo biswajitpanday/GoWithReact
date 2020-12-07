34b110462830a1821c393f8e5b40372e5c421d68
biswajitmailid <biswajitmailid@gmail.com>
2020-12-07 19:48:05 +0000

    Employee store create.

---
+++ reactjs-client/src/features/employee/store.ts
@@ -0,0 +1,50 @@
+import { createSlice, Dispatch } from '@reduxjs/toolkit';
+import { ApiConstant } from '../../helpers/apiConstant';
+import { HttpHelpers } from '../../helpers/httpHelpers';
+import { IEmployeeModel, IEmployeeResponse, IEmployeeStateModel } from './models';
+
+export const defaultEmployeeState: IEmployeeStateModel = {
+    isBusy: false,
+    errorMessage: '',
+    isValidForm: false
+}
+
+const slice = createSlice({
+    name: 'employeeStore',
+    initialState: defaultEmployeeState,
+    reducers: {
+        onError: (state, action) => {
+            return {
+                ...state,
+                isBusy: false,
+                errorMessage: action.payload.data
+            }
+        },
+
+        changeBusyState: (state, action) => {
+            return {
+                ...state,
+                errorMessage: "",
+                isBusy: action.payload.data
+            }
+        }
+    }
+});
+
+export default slice.reducer;
+
+export const employee = (employeeModel: IEmployeeModel) => async (dispatch: Dispatch) => {
+    try {
+        console.log(employeeModel);
+        dispatch(slice.actions.changeBusyState({ data: true }));
+        const response = await HttpHelpers.post<IEmployeeResponse>(ApiConstant.employee, employeeModel);
+        return response;
+    } catch (error) {
+        dispatch(slice.actions.changeBusyState({data: false }));
+        dispatch(slice.actions.onError({ data: error.messages }));
+    }
+}
+
+export const reset = () => async(dispatch: Dispatch) => {
+    dispatch(slice.actions.changeBusyState({ data: false }));
+}
--- reactjs-client/src/helpers/apiConstant.ts
+++ reactjs-client/src/helpers/apiConstant.ts
@@ -2,4 +2,6 @@ export class ApiConstant {
     static login = 'auth/login';
     static registration = 'auth/register';
     static forgetPassword = 'auth/forgetpassword';
+
+    static employee = '/employee';
 }
